e50ec3646b980e5f2dec13234d300b2922bf9a67
Ana Oliveira <ana.silva.oliveira1997@gmail.com>
2026-02-07 18:16:05 +0000

    Initial project setup

---
+++ src/main/kotlin/pt/cinzarosa/ajudante/AjudanteApplication.kt
@@ -0,0 +1,11 @@
+package pt.cinzarosa.ajudante
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AjudanteApplication
+
+fun main(args: Array<String>) {
+	runApplication<AjudanteApplication>(*args)
+}
---
+++ src/test/kotlin/pt/cinzarosa/ajudante/AjudanteApplicationTests.kt
@@ -0,0 +1,13 @@
+package pt.cinzarosa.ajudante
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AjudanteApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
